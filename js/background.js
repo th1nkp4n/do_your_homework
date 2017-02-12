4fcce37d7171dfc9e7d8dd33d3b973a4d7652bed
Kyla Qi <kqi12345@gmail.com>
2017-02-12 04:43:05 -0500

    add page detection actions

--- js/background.js
+++ js/background.js
@@ -3,12 +3,30 @@ var timeLeft= 1800000;
 function checkUpdate(integer tabId, object changeInfo, Tab tab) {    
     if (changeInfo.contains("buzzfeed") {
         chrome.alarms.create("alarm", Date.now() + timeLeft);
+        chrome.alarms.clear("calm");
     } else {
+        var good = true;//checks to see if 
+        alarms.getAll(function(Al) {
+            for (alarm a: Al) {
+                if (a.name == "calm") {
+                    good = false;
+                }
+            }
+        });
         chrome.alarms.get("alarm", function(Alarm al) {
           timeLeft = al.when-Date.now();
         });
         chrome.alarms.clear("alarm");
-        chrome.alarms.create("calm", Date.now() + 1200000);
+        if (good) {
+            chrome.alarms.create("calm", Date.now() + 1200000);
+        }        
     }
     
 }
+chrome.browserAction.onAlarm.addListener(function(Alarm al) {
+      if(al.name.equals("calm")) {
+          timeLeft= 1800000;
+      } else {
+          
+      }
+});
